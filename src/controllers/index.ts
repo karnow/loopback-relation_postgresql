2392ff1b73ad20c7238832baddb2aa3593a7b47e
Karol Nowakowski <karol-nowakowski@o2.pl>
2022-08-07 10:44:10 +0200

    add basic controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,3 +4,7 @@ export * from './order-customer.controller';
 export * from './customer-address.controller';
 export * from './address-customer.controller';
 export * from './order-manufacturer.controller';
+export * from './customer.controller';
+export * from './order.controller';
+export * from './address.controller';
+export * from './manufacturer.controller';
